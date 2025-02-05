b7c27f47969ef9a4192c3452754abc29c231d899
lidcat <>
2025-02-05 23:16:14 +0300

    refactor: редизайн

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-import {productCard} from "./src/components/card.js";
-import {formInput} from "./src/components/formInput.js";
-function main(){
-    const conceptPortfolio = document.getElementById("conceptual-portfolio-store");
-    conceptPortfolio.append(productCard("./public/marketPlace.svg","Маркетплейс мебели и ее подбор"));
-    conceptPortfolio.append(productCard("./public/cosmetica.svg","Концепт магазина косметики"));
-    conceptPortfolio.append(productCard("./public/coto.svg","Концепт кото-кафе"));
-    conceptPortfolio.append(productCard("./public/neon.svg","Компания, производящая неоновые вывески"));
-    conceptPortfolio.append(productCard("./public/cloth.svg","Маркетплейс одежды, с возможностью онлайн торгов"));
-    conceptPortfolio.append(productCard("./public/taxi.svg","Приложение для такси"));
+import { productCard } from "./src/components/card.js";
 
-    const applicationForm = document.getElementById("application-form");
-    applicationForm.append(formInput("Как вас зовут","Ваше имя"));
-    applicationForm.append(formInput("E-mail","Ваша почта"));
-    applicationForm.append(formInput("Контактный номер телефона","Ваша почта"));
-    applicationForm.append(formInput("Где будем обсуждать?","Напишите удобную вам соц. сеть/мессенджер"));
+function main() {
+  const conceptPortfolio = document.getElementById(
+    "conceptual-portfolio-store"
+  );
+  conceptPortfolio.append(
+    productCard("./public/bloom.png", "Bloom (Приложение для знакомств).")
+  );
+  conceptPortfolio.append(
+    productCard(
+      "./public/candles.png",
+      "Lion's Candles(Интернет магазин свечей)"
+    )
+  );
+  conceptPortfolio.append(
+    productCard(
+      "./public/biometric.png",
+      "Bimetric - узконаправленное ПО. Служит для цифровизации столовой"
+    )
+  );
+  conceptPortfolio.append(
+    productCard(
+      "./public/shop.png",
+      "Интернет-магазина мебели, ключевой возможностью было - AR примерка мебели"
+    )
+  );
+  conceptPortfolio.append(
+    productCard(
+      "./public/corp.png",
+      "Разработка корпоративного чата внутри компании."
+    )
+  );
+  conceptPortfolio.append(
+    productCard(
+      "./public/crm.png",
+      "Внутренняя CRM система для управления проектами с аналитикой"
+    )
+  );
 }
 
 main();
--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,9 @@
-export function productCard(img, text){
-    const card = document.createElement('div');
-    card.className = "product-card"
-    card.innerHTML = `
+export function productCard(img, text) {
+  const card = document.createElement("div");
+  card.className = "product-card";
+  card.innerHTML = `
         <img class="product-card-img" src="${img}" alt="product image"/>
-        <p class="dsd-text3 product-card-title">${text}</p>
-    `
-    return card;
+        <p class="tdp-text-1 tdp-white-color product-card-title">${text}</p>
+    `;
+  return card;
 }
